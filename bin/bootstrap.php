e1f9365d1858eaee97a6f8c02c79311b7939fd23
Wi1dcard <wi1dcard.cn@gmail.com>
2018-12-04 14:44:19 +0800

    Format code, add advanced check of swoole extension

--- bin/bootstrap.php
+++ bin/bootstrap.php
@@ -7,23 +7,21 @@
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
+
 define('IN_PHAR', boolval(Phar::running(false)));
-
 define('ROOT', IN_PHAR ? dirname(Phar::running(false)) : realpath(__DIR__ . '/..'));
-
 define('DB_DELIMITER', 'SΜ');
-
 define('SMPROXY_VERSION', IN_PHAR ? '@phar-version@' : absorb_version_from_git());
 
 set_error_handler('_error_handler', E_ALL | E_STRICT);
 
-// 判断php版本
+// Check requirements - PHP
 if (version_compare(PHP_VERSION, '7.0', '<')) {
     smproxy_error('ERROR: PHP version must be greater than 7.0!');
 }
 
-// 判断swoole版本
-if (defined('SWOOLE_VERSION')) {
+// Check requirements - Swoole
+if (extension_loaded('swoole') && defined('SWOOLE_VERSION')) {
     if (version_compare(SWOOLE_VERSION, '2.0', '<')) {
         smproxy_error('ERROR: Swoole version must be greater than 2.1!');
     }
