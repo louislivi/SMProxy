a5dbdb887d090c45860f351487e5e36d3b30f019
louislivi <574747417@qq.com>
2018-12-09 10:16:59 +0800

    Add travis ci test

---
+++ tests/test.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: louislivi
+ * Date: 2018-12-09
+ * Time: 10:10
+ */
+$servername = "127.0.0.1";
+$username = "root";
+$password = "123456";
+$dbname = "mysql";
+$port   = 3366;
+
+// 创建连接
+$conn = new mysqli($servername, $username, $password, $dbname, $port);
+// Check connection
+if ($conn->connect_error) {
+    die("连接失败: " . $conn->connect_error);
+}
+
+$sql = "SELECT * FROM `user`";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // 输出数据
+    while($row = $result->fetch_assoc()) {
+        var_dump($row);
+    }
+} else {
+    echo "0 结果";
+}
+$conn->close();
+?>
